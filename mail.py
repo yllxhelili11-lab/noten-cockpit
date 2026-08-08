ecd644e001ff92abc8fe3741eb485b08b4b77f0a
Il Dje <yllxhelili11@gmail.com>
2026-08-08 03:31:38 +0200

    Gmail-Anmeldung funktioniert

---
+++ mail.py
@@ -0,0 +1,8 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
+flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+creds = flow.run_local_server(port=0)
+
+print("Anmeldung erfolgreich")
